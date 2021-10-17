72bbf4d8c49af7b57e6569bc751aab62a2b6d979
Simon Grchevski <simongrcevski@gmail.com>
2021-10-17 19:55:38 +0200

    Add the basic components

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 export const Header = () => {
   return (
-    <div>
-      This is the header
-    </div>
+    <section>
+      <Link  to="/">
+      <article>
+        <h3>Simon Grcevski</h3>
+        <h5>SG</h5>
+      </article>
+      </Link>
+
+      <article>
+        <Link to="/projects">
+          <h5>Projects</h5>
+        </Link>
+
+        <Link to="/about">
+          <h5>About me</h5>
+        </Link>
+      </article>
+    </section>
   )
 }
---
+++ src/components/landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Landing = () => {
+  return (
+    <div>
+    "THIS SHOULD BE THE MAIN PAGE"
+    </div> 
+  )
+}
