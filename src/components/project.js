817cc026a20e50fa2daefb569fb49b64fc696bef
Simon Grchevski <simongrcevski@gmail.com>
2021-10-17 22:20:21 +0200

    Add the projects into the array

--- src/components/project.js
+++ src/components/project.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export const Project = ({ name }) => {
+export const Project = ({ name, demoUrl, imageUrl, description, technologies, repoLink }) => {
   const [toggleOn, setToggle] = useState(false);
 
   const handleClick = () => {
@@ -9,10 +9,15 @@ export const Project = ({ name }) => {
   }
   return (
     <article onClick ={() => handleClick()}>
-      <img>
+      <img src ={imageUrl}>
       </img>
       This is my project
       name:{name}
+      demoUrl: {demoUrl}
+      imageUrl:{imageUrl}
+      description: {description}
+      technologies: {technologies}
+      repoLink:{repoLink}
       { toggleOn && <div>
       Its Working man</div> }
     </article>
@@ -20,6 +25,18 @@ export const Project = ({ name }) => {
   )
 }
 
+// name={pro.name}
+// demoUrl = {pro.demoUrl}
+// imageUrl = {pro.imageUrl}
+// description = {pro.description}
+// technologies = {pro.technologies}
+// repoLink = {pro.repoLink}
+
 Project.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  demoUrl: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  technologies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  repoLink: PropTypes.string.isRequired
 }
